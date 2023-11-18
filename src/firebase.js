dca595760aaaa7ed2a928e2d407256eb55fe2561
Sandeep Vishwakarma <62471058+sv2441@users.noreply.github.com>
2023-11-18 14:46:29 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,16 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAXqIUzoSPilQxf2Mp96pIgA042pL8mgFo",
-  authDomain: "cluriture.firebaseapp.com",
-  projectId: "it-fsd",
-  storageBucket: "fsd-cluriture.appspot.com",
-  messagingSenderId: "192193417250",
-  appId: "1:192193417250:web:75d881daeb31788fe17a7b",
-  measurementId: "G-F0H1Z0XNQR"
-}; 
 
+const firebaseConfig = {
+  apiKey: "AIzaSyB528WcRDI5R7AVRJqt8eT5navlBW0zdyY",
+  authDomain: "logandsign-e0727.firebaseapp.com",
+  databaseURL: "https://logandsign-e0727-default-rtdb.firebaseio.com",
+  projectId: "logandsign-e0727",
+  storageBucket: "logandsign-e0727.appspot.com",
+  messagingSenderId: "357924532716",
+  appId: "1:357924532716:web:0afdd5bbe0c13f13cfd0c9"
+};
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
